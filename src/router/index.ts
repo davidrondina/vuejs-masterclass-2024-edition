7f9d006d8a0a6b432011537acab1a74d68abfa72
davidrondina <rondindavid.r@gmail.com>
2025-03-03 00:02:00 +0800

    add fallback route

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,5 @@
 import { createRouter, createWebHistory } from "vue-router";
+import { h } from "vue";
 
 const router = createRouter({
   //  Web history enables HTML history mode -- Uses History API to handle navigation
@@ -20,6 +21,28 @@ const router = createRouter({
       path: "/projects/:id",
       name: "single-project",
       component: () => import("@/views/SingleProjectView.vue"),
+    },
+    // Matches undefined routes under /projects route
+    // {
+    //   path: "/projects:catchAll(.*)*",
+    //   name: "NotFound",
+    //   component: h("p", { style: "color:red;" }, "404 project Not Found"),
+    // },
+    /**
+     * This wildcard catches all undefined route
+     *
+     * Breakdown:
+     * /:catchAll - matches any pattern in the root url
+     * (.*) - regex to match anything
+     * * - if this route pattern is repeated for any number of times
+     *
+     * This will match any route that doesn't match any of the above
+     */
+    {
+      path: "/:catchAll(.*)*",
+      name: "NotFound",
+      // h is short for Hyperscript -- JS that produces HTML
+      component: h("p", { style: "color:red;" }, "404 Not Found"),
     },
   ],
 });
