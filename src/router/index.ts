9f76c3affbbab2197bf99bfab242dcace95fd1b2
davidrondina <rondindavid.r@gmail.com>
2025-03-02 20:33:54 +0800

    dynamic route

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,27 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory } from "vue-router";
 
 const router = createRouter({
+  //  Web history enables HTML history mode -- Uses History API to handle navigation
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
-})
+  routes: [
+    {
+      path: "/",
+      name: "home",
+      component: () => import("@/views/HomeView.vue"),
+    },
+    {
+      path: "/projects",
+      name: "projects",
+      component: () => import("@/views/ProjectsView.vue"),
+    },
+    {
+      // Dynamic route
+      // Matches any pattern that places an id in the url
+      path: "/projects/:id",
+      name: "single-project",
+      component: () => import("@/views/SingleProjectView.vue"),
+    },
+  ],
+});
 
-export default router
+export default router;
---
+++ src/views/SingleProjectView.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import { useRoute } from "vue-router";
+
+const router = useRoute();
+</script>
+<template>
+  <div>
+    <!-- Use route.params to get the id from the url path -->
+    <h1>Project {{ router.params.id }}</h1>
+  </div>
+</template>
